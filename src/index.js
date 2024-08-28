de099e01f4682a0a10d5f4ce8660dfbf62ef5cb1
Pratikkadam00 <pratikadam00@gmail.com>
2024-08-28 21:22:56 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { App } from "./App.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Login } from "./components/Login.js";
-import { Home } from "./components/Home";
-import { ProjectUpload } from "./components/ProjectUpload";
-import { Register } from "./components/Register";
-import ProjectCreation from "./components/ProjectCreation";
-import { UploadPage } from "./components/UploadPage";
+import { Home } from "./components/Home.js";
+import { ProjectUpload } from "./components/ProjectUpload.js";
+import { Register } from "./components/Register.js";
+import ProjectCreation from "./components/ProjectCreation.js";
+import { UploadPage } from "./components/UploadPage.js";
 
 const routerConfiguration = createBrowserRouter([
   {
