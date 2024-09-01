56bf0c4ed058b6462099c24d07fb98cae4fd42ee
pratikkadam00 <pratikadam00@gmail.com>
2024-09-01 20:32:24 +0530

    updated

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import NoProjectScreen from "./NoProjectScreen";
+import ProjectScreen from "./ProjectScreen";
+import CreateProjectModal from "./CreateProjectModal";
+import { Project } from "../store/projectStore";
+import { User } from "../store/userStore";
+
+export const Home = () => {
+    const { projectList, isCreateProjectModalOpen, getProjectsWithUserId } =
+        Project((state) => state);
+    const { user } = User((state) => state);
+    const { userId } = user;
+    useEffect(() => {
+        if (!userId) return;
+        getProjectsWithUserId(userId);
+    }, [userId]);
+    return (
+        <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-100">
+            {projectList.length === 0 ? <NoProjectScreen /> : <ProjectScreen />}
+
+            {isCreateProjectModalOpen && <CreateProjectModal />}
+        </div>
+    );
+};
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { User } from "../store/userStore";
+import { useNavigate } from "react-router-dom";
+export const Login = () => {
+    const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
+    const { loginUser, isLoggedIn } = User((state) => state);
+    const onSubmit = (data) => {
+        console.log(data);
+        if (!data.email && !data.password) return;
+        console.log("loginUser(data)", loginUser(data));
+        loginUser(data);
+    };
+    useEffect(() => {
+        if (isLoggedIn) navigate("/home");
+    }, [isLoggedIn]);
+    return (
+        <div className="h-full bg-gray-100 flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+                <h2 className="text-2xl font-bold mb-6 text-center">
+                    Login to Your Account
+                </h2>
+                <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label className="block text-gray-700">Email</label>
+                        <input
+                            type="email"
+                            {...register("email")}
+                            className="w-full p-2 border border-gray-300 rounded mt-1"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-gray-700">Password</label>
+                        <input
+                            type="password"
+                            {...register("password")}
+                            className="w-full p-2 border border-gray-300 rounded mt-1"
+                        />
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            className="w-full bg-purple-600 text-white py-2 rounded"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+                <div className="mt-4 text-center">
+                    <p className="text-gray-600">
+                        Don't have an account?{" "}
+                        <Link to="/register" className="text-purple-600">
+                            Register
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
